029910f5e076369d76356e6471d40c398f389382
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-07-19 00:09:47 +0900

    :recycle: User 엔티티의 정적 팩토리 메소드를 RegisterRequest로 이동

--- src/main/java/woojjam/utrip/domains/auth/dto/request/RegisterRequest.java
+++ src/main/java/woojjam/utrip/domains/auth/dto/request/RegisterRequest.java
@@ -1,6 +1,7 @@
 package woojjam.utrip.domains.auth.dto.request;
 
 import lombok.Data;
+import woojjam.utrip.domains.user.domain.User;
 
 @Data
 public class RegisterRequest {
@@ -10,4 +11,13 @@ public class RegisterRequest {
 	String password;
 	String checkPassword;
 	String role;
+
+	public static User toEntity(String nickname, String email, String password) {
+		return User.builder()
+			.nickname(nickname)
+			.refreshToken(null)
+			.email(email)
+			.password(password)
+			.build();
+	}
 }
