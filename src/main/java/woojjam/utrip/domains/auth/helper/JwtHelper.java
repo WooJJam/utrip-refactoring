154441ea6918346723aa83ad5a910b30eb1d8846
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-07-18 23:17:39 +0900

    :art: Jwt 관련 작업을 담당하는 Helper 클래스 분리

---
+++ src/main/java/woojjam/utrip/domains/auth/helper/JwtHelper.java
@@ -0,0 +1,31 @@
+package woojjam.utrip.domains.auth.helper;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import woojjam.utrip.common.security.jwt.JwtProvider;
+import woojjam.utrip.domains.auth.dto.TokenDto;
+
+@Component
+public class JwtHelper {
+
+	private final JwtProvider accessTokenProvider;
+	private final JwtProvider refreshTokenProvider;
+
+	public JwtHelper(
+		@Qualifier("accessTokenProvider") JwtProvider accessTokenProvider,
+		@Qualifier("refreshTokenProvider") JwtProvider refreshTokenProvider) {
+		this.accessTokenProvider = accessTokenProvider;
+		this.refreshTokenProvider = refreshTokenProvider;
+	}
+
+	public TokenDto generateToken(String email) {
+		String accessToken = accessTokenProvider.generateToken(email);
+		String refreshToken = refreshTokenProvider.generateToken(email);
+		return TokenDto.of(accessToken, refreshToken);
+	}
+
+	public String generateRefreshToken(String email) {
+		return refreshTokenProvider.generateToken("jaemin5548@naver.com");
+	}
+}
